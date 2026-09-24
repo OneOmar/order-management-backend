18fcd685138be8180797f3dcfdd223ef07c4b58b
Omar <elmanssouriomar@gmail.com>
2026-09-24 20:20:49 +0000

    fix(user): ensure enabled=true with Lombok @Builder.Default
    Prevents new users from being created with enabled=false due to Lombok @Builder ignoring default values.
    Fixes login issue "user is disabled".

--- src/main/java/com/ecommerce/order_management/entity/User.java
+++ src/main/java/com/ecommerce/order_management/entity/User.java
@@ -51,6 +51,7 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Role role = Role.ROLE_USER;
 
+    @Builder.Default
     @Column(nullable = false)
     private boolean enabled = true;
 
